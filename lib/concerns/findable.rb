34cc5a28022fa107cb097691b14fe577e2056f66
Simion <simion.okola@student.moringaschool.com>
2022-10-26 10:59:02 +0300

    Completed it

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,11 @@
+module Findable
+    module ClassMethods
+        def find_by_name(name)
+            self.all.detect{|a| a.name == name}
+        end
+
+        def to_param
+            name.downcase.gsub(' ', '-')
+        end
+    end
+end
